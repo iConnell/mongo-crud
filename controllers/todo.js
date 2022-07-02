38407fb6a3808aeec4fe071566cff17d15ca0061
McConnell <ik.ugwuanyi@gmail.com>
2022-07-02 19:02:48 +0100

    added pagination

--- controllers/todo.js
+++ controllers/todo.js
@@ -1,3 +1,4 @@
+const Todo = require("../models/Todo");
 const todo = require("../models/Todo");
 
 const createTodo = async (req, res) => {
@@ -7,49 +8,51 @@ const createTodo = async (req, res) => {
 };
 
 const getTodo = async (req, res, next) => {
-  const { id: TodoID } = req.params;
-  const todo = await Todo.findOne({ _id: TodoID });
+  const { id: todoID } = req.params;
+  const todo = await Todo.findOne({ _id: todoID });
   if (!todo) {
     return res
       .status(404)
-      .json({ msg: `Todo with the ID ${TodoID} not found` });
+      .json({ msg: `Todo with the ID ${todoID} not found` });
   }
 
-  res.status(200).json({ Todo });
+  res.status(200).json({ todo });
 };
 
 const getAllTodods = async (req, res) => {
-  const todos = await Todo.find({});
+  let todos = Todo.find({});
 
   const limit = Number(req.query.limit) || 10;
   const page = Number(req.query.page) || 1;
   const skip = (page - 1) * limit;
 
-  res.status(200).json({ Todos, count: Todos.length });
+  let result = await todos.skip(skip).limit(limit);
+
+  res.status(200).json({ result, count: result.length });
 };
 
 const updateTodo = async (req, res) => {
-  const { id: TodoID } = req.params;
+  const { id: todoID } = req.params;
 
-  Todo = await Todo.findByIdAndUpdate({ _id: TodoID }, req.body, {
+  const todo = await Todo.findByIdAndUpdate({ _id: todoID }, req.body, {
     runValidators: true,
     new: true,
   });
   if (!todo) {
     return res
       .status(404)
-      .json({ msg: `Todo with the ID ${TodoID} not found` });
+      .json({ msg: `Todo with the ID ${todoID} not found` });
   }
-  res.status(200).json({ Todo });
+  res.status(200).json({ todo });
 };
 
 const deleteTodo = async (req, res) => {
-  const { id: TodoID } = req.params;
-  Todo = await Todo.findByIdAndDelete({ _id: TodoID });
+  const { id: todoID } = req.params;
+  const todo = await Todo.findByIdAndDelete({ _id: todoID });
   if (!todo) {
     return res
       .status(404)
-      .json({ msg: `Todo with the ID ${TodoID} not found` });
+      .json({ msg: `Todo with the ID ${todoID} not found` });
   }
   res.status(200).send();
 };
